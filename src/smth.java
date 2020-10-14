8117a2977e3f36700ec67effd1fcee09af5d6c40
MARk3108 <mark.fomin.2003@gmail.com>
2020-10-14 21:38:03 +0300

    first commit

---
+++ src/smth.java
@@ -0,0 +1,13 @@
+public class smth {
+    private String name;
+    public void setName(String str) {
+        name = str;
+    }
+    public String getName(){
+        return name;
+    }
+    public boolean equals(String getted){
+        return (name.equals(getted));
+    }
+
+}
---
+++ src/task4.java
@@ -0,0 +1,9 @@
+public class task4 {
+    public static void main(String[] args) {
+        smth chel = new smth();
+        String str="Вова";
+        chel.setName(str);
+        String getted = chel.getName();
+        System.out.println(chel.equals(getted));
+    }
+}
